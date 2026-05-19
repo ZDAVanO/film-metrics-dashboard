fd6d98a616ca57f031dbc453db457082e503c9a2
Oleh <olehivaniuk.net@gmail.com>
2026-05-19 12:37:51 +0300

    Update theme-provider.tsx

--- dashboard/components/theme-provider.tsx
+++ dashboard/components/theme-provider.tsx
@@ -7,5 +7,9 @@ export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props} attribute="class" defaultTheme="dark" forcedTheme="dark">
+      {children}
+    </NextThemesProvider>
+  )
 }
